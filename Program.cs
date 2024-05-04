e4639634d23ce03c92fc7bfddc578eaa6faa271b
Richard <68912027+RichardFPereira@users.noreply.github.com>
2024-05-04 22:45:11 +0000

    Criado `Program.cs` para rodar o programa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using System.Text;
+using _Sistema_de_Hospedagem.Models;
+using _Sistema_de_Hospedagem.Enum;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(Nome: "José", Sobrenome:"da Silva");
+Pessoa p2 = new Pessoa(Nome: "Maria", Sobrenome:"do Socorro");
+
+hospedes.Add(p1);
+
+Suite suite = new Suite(TipoSuiteEnum.PREMIUM, Capacidade: 2, ValorDiaria: 30);
+
+Reserva reserva = new Reserva(Hospedes: hospedes, Acomodacao: suite, DiasReserva: 10);
+reserva.CadastrarSuite(suite);
+
+hospedes.Add(p2);
+reserva.CadastrarHospedes(hospedes);
+
+Console.WriteLine($"Hóspedes: {reserva.ObterQantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
