5de073059f3f97c2d67740ec80f4ae107f48e884
Richard <68912027+RichardFPereira@users.noreply.github.com>
2024-05-04 02:50:10 +0000

    Criada regra de negócio do objeto `Pessoa.cs`

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -7,6 +7,54 @@ namespace _Sistema_de_Hospedagem.Models
 {
     public class Pessoa
     {
+        private string nome;
+        private string sobrenome;
         
+        public Pessoa (string Nome, string Sobrenome)
+        {
+            if (string.IsNullOrEmpty(Nome))
+            {
+                throw new ArgumentException($"Nome inválido.");
+            }
+            if (string.IsNullOrEmpty(Sobrenome))
+            {
+                throw new ArgumentException($"Sobrenome inválido.");
+            }
+
+            this.nome = Nome.ToUpper();
+            this.sobrenome = Sobrenome.ToUpper();
+        }        
+        public string Nome 
+        { 
+            get => nome;
+            
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Nome inválido.");
+                }
+                else 
+                {
+                    nome = value.ToUpper();
+                }
+            }         
+        }
+        public string Sobrenome 
+        { 
+            get => sobrenome; 
+            
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Sobrenome inválido.");
+                }
+                else
+                {
+                    sobrenome = value.ToUpper();
+                }
+            } 
+        }
     }
 }
